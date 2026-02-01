e7cc073ca0344e9eff03b09a8b36b4e4df4bd534
Neha Negi <neha.negi0013@gmail.com>
2026-02-01 23:07:42 +0530

    Added Navbar, ResumeCard and updated routes

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router";
+import type { reactRouter } from "@react-router/dev/vite";
+const ResumeCard = ({
+  resume: { id, companyName, jobTitle, feedback },
+}: {
+  resume: Resume;
+}) => {
+  return (
+    <Link
+      to={"/resume/${resume.id}"}
+      className="resume-card animate-in fade-in duration-1000"
+    >
+      <div className="flex flex-col gap-2">
+        <h2 className="text-black  font-bold wrap-break-words">
+          {companyName}{" "}
+        </h2>
+        <h3 className="text-lg  wrap-break-words text-gray-500">{jobTitle}</h3>
+      </div>
+      <div className="shrink-0"></div>
+    </Link>
+  );
+};
+
+export default ResumeCard;
