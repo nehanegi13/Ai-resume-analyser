cc73a73edc3306d2521e5da87e95706b114ca014
Neha Negi <neha.negi0013@gmail.com>
2026-02-02 18:02:00 +0530

    updated Auth by adding useLocation and useNavigate

--- app/routes/Auth.tsx
+++ app/routes/Auth.tsx
@@ -1,4 +1,6 @@
 import { usePuterStore } from "lib/puter";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 
 export const meta = () => [
   { title: "Resumind | Auth" },
@@ -7,6 +9,14 @@ export const meta = () => [
 
 const Auth = () => {
   const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next]);
+
   return (
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div className="gradient-border shadow-lg ">
@@ -24,11 +34,11 @@ const Auth = () => {
               <>
                 {auth.isAuthenticated ? (
                   <button className="auth-button" onClick={auth.signOut}>
-                    Log Out
+                    <p> Log Out </p>
                   </button>
                 ) : (
                   <button className="auth-button" onClick={auth.signIn}>
-                    Log In
+                    <p>Log In</p>
                   </button>
                 )}
               </>
