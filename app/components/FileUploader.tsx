f09266abce59273c3fe1fb8b4c8901a6dbc4e2b2
Neha Negi <neha.negi0013@gmail.com>
2026-02-04 19:14:07 +0530

    imported usecallback hook from react and usedropzone from react-dropzone

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
-
 const FileUploader = () => {
   return <div className="w-full gradient-border">File Uploader</div>;
 };
