1f709f3d507bf589b7272a344325bef829f03fdf
Neha Negi <neha.negi0013@gmail.com>
2026-02-04 19:20:18 +0530

    drop and down function made

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,8 +1,24 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
+
 const FileUploader = () => {
-  return <div className="w-full gradient-border">File Uploader</div>;
+   const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  return <div className="w-full gradient-border"> 
+  
+   <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  
+  </div>;
 };
 
 export default FileUploader;
