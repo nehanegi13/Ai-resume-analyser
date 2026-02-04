e1510879507c18a4a5027c80f226717703c4ba0d
Neha Negi <neha.negi0013@gmail.com>
2026-02-04 19:12:16 +0530

    downloaded react-dropzone component

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,9 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+
+const FileUploader = () => {
+  return <div className="w-full gradient-border">File Uploader</div>;
+};
+
+export default FileUploader;
