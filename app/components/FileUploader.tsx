38d1fe5e675b93cd81d9073dc2dd5d246bad205e
Neha Negi <neha.negi0013@gmail.com>
2026-02-04 19:35:41 +0530

    tailwind given to the drag drop button

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,24 +1,35 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
-
 const FileUploader = () => {
-   const onDrop = useCallback(acceptedFiles => {
+  const [file, setFile] = useState();
+  const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-  return <div className="w-full gradient-border"> 
-  
-   <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  return (
+    <div className="w-full gradient-border">
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className="space-y-4 cursor-pointer">
+          <div className="mx-auto w-16 h-16 flex item-center justify-center">
+            <img src="/icons/info.svg" alt="upload" className="size-20" />
+          </div>
+          {file ? (
+            <div></div>
+          ) : (
+            <div>
+              <p className="text-lg text-gray-500">
+                <span className="font-semibold">Click to Upload</span> or drag
+                and drop
+              </p>
+              <p className="text-lg text-gray-500">PDF (max 20 MB)</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  
-  </div>;
+  );
 };
 
 export default FileUploader;
